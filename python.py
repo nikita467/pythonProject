61973d235c8d73f8b4a77f8a8c62e86527569217
nikita467 <cerkasovnikita471@gmail.com>
2023-01-29 11:53:10 +0200

    dz

--- python.py
+++ python.py
@@ -1,1 +1,36 @@
-print
+import random
+class Student:
+    def __init__(self,name):
+        self.name = name
+        self.gladness = 50
+        self.progress = 0
+        self.alive = True
+
+
+    def to_study(self):
+        print("Time to study")
+        self.progress += 0.12
+        self.gladness -= 3
+
+
+    def to_sleep(self):
+        print("I will sleep")
+        self.gladness += 3
+
+
+    def to_chill(self):
+        print("Rest time")
+        self.gladness += 5
+        self.progress -= 0.1
+
+
+    def to_alive(self):
+        if self.progress < -0.5:
+            print("Cash out..")
+            self.alive = False
+        elif self.gladness <= 0:
+            print("Depresion")
+            self.alive = False
+        elif self.progress > 5:
+            print("Passed externally")
+            self.alive = False
