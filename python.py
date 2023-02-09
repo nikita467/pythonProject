833ae69463b7fd7e77d232f1076a04635e8c964c
nikita467 <cerkasovnikita471@gmail.com>
2023-02-09 22:45:03 +0200

    dz2

--- python.py
+++ python.py
@@ -11,7 +11,10 @@ class Student:
         print("Time to study")
         self.progress += 0.12
         self.gladness -= 3
-
+    def to_work(self):
+        print("I need go work")
+        self.progress -= 5
+        self.gladness += 2
 
     def to_sleep(self):
         print("I will sleep")
@@ -34,3 +37,23 @@ class Student:
         elif self.progress > 5:
             print("Passed externally")
             self.alive = False
+
+    def end_of_day(self):
+        print(f"Gladness = {self.gladness}")
+        print(f"Progress = {round(self.progress, 2)}")
+    def life(self, day):
+        day = "Day" + str(day) + "of" + self.name + "life"
+        print(f"{day:=^50}")
+        life_cube = random.randint(1, 3)
+        if life_cube == 1:
+            self.to_study()
+        elif life_cube == 2:
+            self.to_sleep()
+        elif life_cube == 3:
+            self.to_chill()
+        self.end_of_day()
+nick = Student(name="Nick")
+for day in range(365):
+    if nick.alive == False:
+        break
+    nick.life(day)
